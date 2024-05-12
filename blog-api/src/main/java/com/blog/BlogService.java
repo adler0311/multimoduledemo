ce5d7d2a0d59e5a4632d890d8cb83dcb7f5da91f
wonho <adler0311@gmail.com>
2024-05-12 15:10:20 +0900

    db setting

--- blog-api/src/main/java/com/blog/BlogService.java
+++ blog-api/src/main/java/com/blog/BlogService.java
@@ -9,7 +9,7 @@ public class BlogService {
     public final ConfirmRepository confirmRepository;
 
     public Long createBlog() {
-        Confirm confirm = confirmRepository.saveAndFlush(Confirm.builder().id(1L).description("test2").build());
+        Confirm confirm = confirmRepository.saveAndFlush(Confirm.builder().id(1L).description("test222").build());
         return confirm.getId();
     }
 }
